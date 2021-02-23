8b08cac31a970636264e5c62784b828e43f1832d
Steele Scott <60533996+steele123@users.noreply.github.com>
2021-02-23 18:55:38 -0500

    setting up base project

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using Stonkbot.Core.Data;
+using Stonkbot.Utils;
+
+namespace Stonkbot
+{
+    public class Program
+    {
+        private DiscordSocketClient _client;
+        private const string configPath = "Resources/config.json";
+        private const string defaultToken = "YOUR_TOKEN HERE";
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            await _client.LoginAsync(TokenType.Bot, GetConfig().token);
+            await _client.StartAsync();
+
+            // This will make task wait until program is closed
+            await Task.Delay(-1);
+        }
+
+        // TODO: Make this properly handle a new config.
+        private static Config GetConfig()
+        {
+            if (!File.Exists(configPath))
+            {
+                Logger.Log($"Couldn't find a config file at '{configPath}'");
+                CreateDefaultConfig();
+            }
+
+            string json = File.ReadAllText(configPath);
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                Logger.Log($"Couldn't find a valid config in '{configPath}'");
+                throw new ConfigException();
+            }
+
+            var config = JsonConvert.DeserializeObject<Config>(json);
+            if (config.token != defaultToken && !string.IsNullOrWhiteSpace(config.token))
+            {
+                return config;
+            }
+            else
+            {
+                Logger.LogError($"Couldn't find a valid token in '{config.token}'");
+                throw new ConfigException();
+            }
+        }
+
+        private static void CreateDefaultConfig()
+        {
+            var config = new Config { token = defaultToken };
+            File.WriteAllText(configPath, JsonConvert.SerializeObject(config));
+        }
+    }
+}
---
+++ Utils/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Stonkbot.Utils
+{
+    public enum LogType
+    {
+        Info,
+        Warning,
+        Error,
+    }
+
+    public static class Logger
+    {
+        public static void Log(LogType  logType, string message)
+        {
+            Console.WriteLine($"[{logType.ToString().ToUpper()}] {message}");
+        }
+
+        public static void Log(string message)
+        {
+            Console.WriteLine("[INFO] " + message);
+        }
+
+        public static void LogWarning(string message)
+        {
+            Console.WriteLine("[WARNING] " + message);
+        }
+
+        public static void LogError(string message)
+        {
+            Console.WriteLine("[ERROR] " + message);
+        }
+    }
+}
